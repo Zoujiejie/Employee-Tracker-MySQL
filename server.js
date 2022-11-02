106fb739688da1d61bb436daacbc8f509e3f7586
Jie Zou <jz3134@columbia.edu>
2022-11-01 22:14:49 -0700

    first test

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const inquirer = require("inquirer");
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Use dotenv to protect password
 require('dotenv').config()
-const connection = mysql.createConnection({
+const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: process.env.DB_PASSWORD,
@@ -21,24 +21,16 @@ const connection = mysql.createConnection({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// Start server after DB connection
-db.connect(err => {
-    if (err) throw err;
-    app.listen(PORT, () => { });
-});
-
-// Start the prompt functions
 function startPrompt() {
     inquirer.prompt({
         type: 'list',
         name: 'menu',
         message: 'What would you like to do?',
-        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete Department', 'Delete Role', 'Delete Employee'],
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Update An Employee Manager', 'Delete a Department', 'Delete a Role', 'Delete an Employee', 'No Action'],
 
     }).then(answer => {
         switch (answer.menu) {
@@ -75,11 +67,12 @@ function startPrompt() {
             case 'Delete Employee':
                 deleteEmployee();
                 break;
+            case 'No Action':
+                connection.end();
         }
     })
 };
 
-// View all departments
 function viewAllDepartments() {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, result) => {
@@ -92,7 +85,6 @@ function viewAllDepartments() {
     });
 };
 
-// View all roles
 function viewAllRoles() {
     const sql = `SELECT * FROM role`;
     db.query(sql, (err, result) => {
@@ -105,13 +97,12 @@ function viewAllRoles() {
     });
 };
 
-// View all employees
 function viewAllEmployees() {
     const sql = `SELECT employee.id,
                 employee.first_name,
                 employee.last_name,
                 role.title AS job_title,
-                department.department_name,
+                department_name AS department,
                 role.salary,
                 CONCAT(manager.first_name, ' ' ,manager.last_name) AS manager
                 FROM employee
@@ -126,13 +117,12 @@ function viewAllEmployees() {
     });
 };
 
-// Add departments
 function addDepartment() {
     inquirer.prompt([
         {
             name: "department_name",
             type: "input",
-            message: "Please enter the name of the department you want to add to the database."
+            message: "Please enter the name of the department you want to add."
         }
     ]).then((answer) => {
 
@@ -141,7 +131,7 @@ function addDepartment() {
         const params = [answer.department_name];
         db.query(sql, params, (err, result) => {
             if (err) throw err;
-            console.log('The new department entered has been added successfully to the database.');
+            console.log('The new department entered has been successfully added.');
 
             db.query(`SELECT * FROM department`, (err, result) => {
                 if (err) {
@@ -155,28 +145,27 @@ function addDepartment() {
     });
 };
 
-// Add a role
 function addRole() {
     inquirer.prompt([
         {
             name: "title",
             type: "input",
-            message: "Please enter the title of role you want to add to the database."
+            message: "Please enter the title of role you want to add."
         },
         {
             name: "salary",
             type: "input",
-            message: "Please enter the salary associated with the role you want to add to the database. (no dots, space or commas)"
+            message: "Please enter the salary associated with the role you want to add (no comma between numbers)."
         },
         {
             name: "department_id",
             type: "number",
-            message: "Please enter the department's id associated with the role you want to add to the database."
+            message: "Please enter the department's id associated with the role you want to add."
         }
     ]).then(function (response) {
         db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [response.title, response.salary, response.department_id], function (err, data) {
             if (err) throw err;
-            console.log('The new role entered has been added successfully to the database.');
+            console.log('The new role entered has been successfully added.');
 
             db.query(`SELECT * FROM role`, (err, result) => {
                 if (err) {
@@ -190,34 +179,33 @@ function addRole() {
     });
 };
 
-// Add employees
 function addEmployee() {
     inquirer.prompt([
         {
             name: "first_name",
             type: "input",
-            message: "Please enter the first name of the employee you want to add to the database."
+            message: "Please enter the first name of the employee you want to add."
         },
         {
             name: "last_name",
             type: "input",
-            message: "Please enter the last name of the employee you want to add to the database."
+            message: "Please enter the last name of the employee you want to add."
         },
         {
             name: "role_id",
             type: "number",
-            message: "Please enter the role id associated with the employee you want to add to the database. Enter ONLY numbers."
+            message: "Please enter the role id associated with the employee you want to add. Integers only."
         },
         {
             name: "manager_id",
             type: "number",
-            message: "Please enter the manager's id associated with the employee you want to add to the database. Enter ONLY numbers."
+            message: "Please enter the manager's id associated with the employee you want to add. Integers only"
         }
 
     ]).then(function (response) {
         db.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", [response.first_name, response.last_name, response.role_id, response.manager_id], function (err, data) {
             if (err) throw err;
-            console.log('The new employee entered has been added successfully to the database.');
+            console.log('The new employee entered has been successfully added.');
 
             db.query(`SELECT * FROM employee`, (err, result) => {
                 if (err) {
@@ -231,23 +219,22 @@ function addEmployee() {
     });
 };
 
-// Update employee role
 function updateEmployeeRole() {
     inquirer.prompt([
         {
             name: "first_name",
             type: "input",
-            message: "Please enter the first name of the employee you want update in the database."
+            message: "Please enter the first name of the employee you want update."
         },
         {
             name: "role_id",
             type: "number",
-            message: "Please enter the new role number id associated with the employee you want to update in the database. Enter ONLY numbers."
+            message: "Please enter the new role id associated with the employee you want to update. Integers only."
         }
     ]).then(function (response) {
         db.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [response.role_id, response.first_name], function (err, data) {
             if (err) throw err;
-            console.log('The new role entered has been added successfully to the database.');
+            console.log('The new role entered has been successfully added to the database.');
 
             db.query(`SELECT * FROM employee`, (err, result) => {
                 if (err) {
@@ -261,23 +248,22 @@ function updateEmployeeRole() {
     });
 };
 
-// Update employee manager
 function updateEmployeeManager() {
     inquirer.prompt([
         {
             name: "first_name",
             type: "input",
-            message: "Please enter the first name of the employee you want update in the database."
+            message: "Please enter the first name of the employee you want update."
         },
         {
             name: "manager_id",
             type: "number",
-            message: "Please enter the new manager's id number associated with the employee you want to update in the database. Enter ONLY numbers."
+            message: "Please enter the new manager's id number associated with the employee you want to update. Integers only."
         }
     ]).then(function (response) {
         db.query("UPDATE employee SET manager_id = ? WHERE first_name = ?", [response.manager_id, response.first_name], function (err, data) {
             if (err) throw err;
-            console.log("The new manager's id entered has been added successfully to the database.");
+            console.log("The new manager's id entered has been successfully added.");
 
             db.query(`SELECT * FROM employee`, (err, result) => {
                 if (err) {
@@ -291,18 +277,17 @@ function updateEmployeeManager() {
     });
 };
 
-// Delete department
 function deleteDepartment() {
     inquirer.prompt([
         {
             name: "department_id",
             type: "number",
-            message: "Please enter the id of the department you want to delete from the database. Enter ONLY numbers."
+            message: "Please enter the id of the department you want to delete. Integers only."
         }
     ]).then(function (response) {
         db.query("DELETE FROM department WHERE id = ?", [response.department_id], function (err, data) {
             if (err) throw err;
-            console.log("The department entered has been deleted successfully from the database.");
+            console.log("The department entered has been successfully deleted.");
 
             db.query(`SELECT * FROM department`, (err, result) => {
                 if (err) {
@@ -316,18 +301,17 @@ function deleteDepartment() {
     });
 };
 
-// Delete role
 function deleteRole() {
     inquirer.prompt([
         {
             name: "role_id",
             type: "number",
-            message: "Please enter the id of the role you want to delete from the database. Enter ONLY numbers."
+            message: "Please enter the id of the role you want to delete. Integers only."
         }
     ]).then(function (response) {
         db.query("DELETE FROM role WHERE id = ?", [response.role_id], function (err, data) {
             if (err) throw err;
-            console.log("The role entered has been deleted successfully from the database.");
+            console.log("The role entered has been successfully deleted from the database.");
 
             db.query(`SELECT * FROM role`, (err, result) => {
                 if (err) {
@@ -341,18 +325,17 @@ function deleteRole() {
     });
 };
 
-// Delete Employee
 function deleteEmployee() {
     inquirer.prompt([
         {
             name: "employee_id",
             type: "number",
-            message: "Please enter the id of the employee you want to delete from the database. Enter ONLY numbers."
+            message: "Please enter the id of the employee you want to delete. Integers only."
         }
     ]).then(function (response) {
         db.query("DELETE FROM employee WHERE id = ?", [response.employee_id], function (err, data) {
             if (err) throw err;
-            console.log("The employee entered has been deleted successfully from the database.");
+            console.log("The employee entered has been successfully deleted.");
 
             db.query(`SELECT * FROM employee`, (err, result) => {
                 if (err) {
